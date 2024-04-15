fd1228a951a4bd6a3e8d067b30d31b304dd2c280
Firas Jemaa <firas.jemaa@yahoo.fr>
2024-04-15 14:12:32 +0200

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TaskMastery
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
