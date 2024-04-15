2f02a4ef66017a3489bfa1549183c47c45bd3c1d
Firas Jemaa <firas.jemaa@yahoo.fr>
2024-04-15 16:09:14 +0200

    Premiere version

---
+++ assets/Components/ConnexionUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TaskMastery.Log.Components
+{
+    /// <summary>
+    /// Logique d'interaction pour ConnexionUserControl.xaml
+    /// </summary>
+    public partial class ConnexionUserControl : UserControl
+    {
+        public ConnexionUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
